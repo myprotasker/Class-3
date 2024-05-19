5c0f9bc839a6b3ab0de721245d78e75a27ba54f3
myprotasker <muhammadaqeelemail@gmail.com>
2024-05-19 01:15:26 -0500

    hello world leap year project

---
+++ leap_year.dart
@@ -0,0 +1,12 @@
+void main (){
+  int year = 2024;
+
+  if (year%4 == 0) {
+    
+    print (year);
+    print ('is a leap year');
+  } else {
+    print (year);
+    print ('is not a leap year');
+  }
+}
